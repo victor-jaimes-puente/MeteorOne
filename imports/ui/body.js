985f1ba21058c5c33bbc5421e753a6a73e36566d
VictorJP <victor.jaimes.puente@gmail.com>
2020-06-23 21:24:00 -0500

    added hide completed, cound uncompleted, working on user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,26 @@
 import { Template } from 'meteor/templating';
-
 import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import './task.js'
 import './body.html';
 
+//ReactiveDict
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
     tasks() {
-        return Tasks.find({}, {sort: {createdAt: -1 }});
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            // If hide completed is checked, filter tasks
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+    },
+    incompleteCount() {
+        return Tasks.find({ checked: { $ne: true } }).count();
     },
 });
 Template.body.events({
@@ -28,5 +41,8 @@ Template.body.events({
 
         // clear form
         target.text.value = '';
-    }
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
 })
